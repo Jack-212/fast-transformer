13fad75638c8d0f5b0a2c335f10385ba2ea5500c
Miso <jcmcdonald96@gmail.com>
2020-02-03 21:38:04 +0800

    Renamed handler for clarity

---
+++ proc_handler.py
@@ -0,0 +1,34 @@
+import transformer_tokenizer
+import transformer_vocab
+from fastai.text import Tokenizer, NumericalizeProcessor, TokenizeProcessor
+from transformers import AutoConfig,AutoModel, AutoModelForSequenceClassification, AutoTokenizer
+from functools import partial
+
+class ProcHandler():
+    def __init__(self):
+        self.tokenizer_class = AutoTokenizer
+        self.config_class = AutoConfig
+        self.model_class = AutoModel
+
+    def fetch_pretrained(self, class_obj) -> object:
+        pretrained = class_obj.from_pretrained(self.model_name)
+        return pretrained
+
+class SeqClassHandler(ProcHandler):
+    def __init__(self, model_name):
+        super().__init__()
+        setattr(self, 'model_class', AutoModelForSequenceClassification)
+        self.model_name = model_name
+        self.pt_tokenizer, self.pt_config, self.pt_model = [self.fetch_pretrained(i) for i in [self.tokenizer_class,self.config_class,self.model_class]]
+        self.base_tokenizer = transformer_tokenizer.TransformersBaseTokenizer(self.model_name, self.pt_tokenizer)
+        self.tokenizer = Tokenizer(tok_func=self.base_tokenizer, pre_rules= [], post_rules= [])
+        self.vocab = transformer_vocab.TransformersVocab(self.pt_tokenizer)
+    
+    def __repr__(self):
+        return f'Model: {self.model_name}\nOriginal Tokenizer: {self.pt_tokenizer}'
+
+    def __call__(self):
+        # Processors for Tokenising & Processing
+        tokenizer_proc = TokenizeProcessor(tokenizer = self.tokenizer, include_bos=False,include_eos=False)
+        numerilize_proc = NumericalizeProcessor(vocab = self.vocab)
+        return [tokenizer_proc, numerilize_proc]
---
+++ transformer_vocab.py
@@ -0,0 +1,31 @@
+from fastai.text import Vocab
+import numpy as np
+from typing import List, Collection
+import collections
+
+class TransformersVocab(Vocab):
+    # itos is a list of token vocab
+    # stoi is an id:tok dictionary, built below
+    # Overwrite base functions to use pt_tokenizer behaviour
+    def __init__(self, pretrained_tokenizer):
+        super().__init__(itos = [])
+        self.tokenizer = pretrained_tokenizer
+    
+    def numericalize(self, t:Collection[str]) -> List[int]:
+        "Convert a list of tokens `t` to their ids."
+        return self.tokenizer.convert_tokens_to_ids(t)
+
+    def textify(self, nums:Collection[int], sep=' ') -> List[str]:
+        "Convert a list of `nums` to their tokens."
+        nums = np.array(nums).tolist()
+        return sep.join(self.tokenizer.convert_ids_to_tokens(nums)) if sep is not None\
+        else self.tokenizer.convert_ids_to_tokens(nums)
+    
+    # Following two are direct from Vocab Object.
+    def __getstate__(self):
+        return {'itos':self.itos, 'tokenizer':self.tokenizer}
+
+    def __setstate__(self, state:dict):
+        self.itos = state['itos']
+        self.tokenizer = state['tokenizer']
+        self.stoi = collections.defaultdict(int,{v:k for k,v in enumerate(self.itos)})
