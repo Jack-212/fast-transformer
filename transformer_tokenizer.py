c47f1da163d942364e24a80516775cafac2af1ea
Miso <jcmcdonald96@gmail.com>
2020-02-18 22:36:51 +0800

    Adjusted tokenizer and added data folder

--- transformer_tokenizer.py
+++ transformer_tokenizer.py
@@ -7,8 +7,10 @@ class TransformersBaseTokenizer(BaseTokenizer):
     """Wraps transformer pretrained tokenizer
         Inherits from BaseTokenizer for fast.ai compatibility"""
     
-    def __init__(self, model_name, pretrained_tokenizer):
+    def __init__(self, model_name, pt_tokenizer):
         self.model_name = model_name
+        self.pt_tokenizer = pt_tokenizer
+        self.max_seq_len = pt_tokenizer.max_len
 
     def __name__(self):
         return f'{self.model_name}_BaseTokenizer'
@@ -19,18 +21,18 @@ class TransformersBaseTokenizer(BaseTokenizer):
     # tokenizer instance passed through to Tokenizer tok_func
     def tokenizer(self, t:str) -> List[str]:
         
-        cls_token = self.pretrained_tokenizer.cls_token # classification token (bos)
+        cls_token = self.pt_tokenizer.cls_token # classification token (bos)
 
-        sep_token = self.pretrained_tokenizer.sep_token # seperation token
+        sep_token = self.pt_tokenizer.sep_token # seperation token
         
 
         # Roberta requires a space to start the input string (add prefix_space) (Inherits from GPT2Tokenizer)
         if self.model_name in list(ROBERTA_PRETRAINED_CONFIG_ARCHIVE_MAP):
 
             #Take 2 from max_seq_len to account for the cls & sep tokens we add to the sequence
-            tokens = self.pretrained_tokenizer.tokenize(t, add_prefix_space=True)[:self.max_seq_len - 2]
+            tokens = self.pt_tokenizer.tokenize(t, add_prefix_space=True)[:self.max_seq_len - 2]
             
         else:
-            tokens = self.pretrained_tokenizer.tokenize(t)[:self.max_seq_len - 2]
+            tokens = self.pt_tokenizer.tokenize(t)[:self.max_seq_len - 2]
     
         return [cls_token] + tokens + [sep_token]
